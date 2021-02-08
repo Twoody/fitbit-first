cd9100e313f85f224c8eb14ad13aee47c5a891b9
twoody <tanner.l.woody@gmail.com>
2021-02-07 23:13:26 -0800

    sleep util

--- common/utils.js
+++ common/utils.js
@@ -5,3 +5,12 @@ export function zeroPad(i) {
   }
   return i;
 }
+
+/**
+ * Sometimes we want delay in our code; This does that.
+ * @return {Promise} -- Given amount of time to wait
+ */
+export function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
